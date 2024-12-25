e3cc3432a0aa2911496eb80b859e04daba34c38c
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-25 15:52:59 +0800

    add root `/package` route

---
+++ apps/docs/src/routes/package/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { data } = $props();
+  const { packages } = data;
+</script>
+
+<h1>Packages</h1>
+
+{#each packages as [_name, pkg]}
+	<div class="package-card">
+	<a href={`/package/${globalThis.encodeURIComponent(pkg.name)}`}><h2>{pkg.name}</h2></a>
+	<p class="description">{pkg.description}</p>
+	<div class="version">Version: {pkg.version}</div>
+	</div>
+{/each}
