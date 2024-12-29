7d9d1cb046c80edf68eb440ba066dfcbbfae2b98
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-29 13:42:19 +0800

    change to early throw

--- apps/docs/src/routes/package/[name]/+page.server.ts
+++ apps/docs/src/routes/package/[name]/+page.server.ts
@@ -3,21 +3,19 @@ import { error } from "@sveltejs/kit";
 import { building } from "$app/environment";
 
 export async function load(ev) {
+	if (!building) throw new Error("Unreachable");
+	const [path, sveltepress] = await Promise.all([
+		import("node:path"),
+		import("@sveltepress/vite"),
+	]);
 	const { params, parent } = ev;
 	const { packages } = await parent();
 	const name = globalThis.decodeURIComponent(params.name);
 	const pkg = packages.get(name);
 	if (!pkg) throw error(404, `Unrecognized package name "${name}"`);
-	if (building) {
-		const [path, sveltepress] = await Promise.all([
-			import("node:path"),
-			import("@sveltepress/vite"),
-		]);
-		const html = await sveltepress.mdToSvelte({
-			mdContent: pkg.readme,
-			filename: path.join(pkg.dirname, "README.md"),
-		});
-		return { pkg, html };
-	}
-	throw new Error("Unreachable");
+	const html = await sveltepress.mdToSvelte({
+		mdContent: pkg.readme,
+		filename: path.join(pkg.dirname, "README.md"),
+	});
+	return { pkg, html };
 }
