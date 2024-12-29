f324f15bd80724ee480083c31ff941ef4ef2a984
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-29 13:29:59 +0800

    attempt to fix issue with `"pathe".win32"`

--- apps/docs/src/routes/package/+layout.server.ts
+++ apps/docs/src/routes/package/+layout.server.ts
@@ -1,29 +1,41 @@
-import normalize from "normalize-package-data";
-import path from "pathe";
+import type { Package as NormalizedPkgData } from "normalize-package-data";
+
+import { building } from "$app/environment";
 
 const ORG_NAME = "@svelte-docgen";
-const PREFIX_EXCEPTIONS = new Set(["svelte-docgen", "vite-plugin-svelte-docgen"]);
+const PREFIX_EXCEPTIONS = new Set([
+	"svelte-docgen",
+	"vite-plugin-svelte-docgen",
+]);
 
 interface Package {
-	data: normalize.Package;
+	data: NormalizedPkgData;
 	dirname: string;
 	readme: string;
 }
 
 export async function load() {
-	const glob_packages = import.meta.glob("../../../../../packages/**/package.json", {
-		import: "default",
-	});
+	if (!building) throw new Error("Unreachable");
+	const [{ default: normalize }, path] = await Promise.all([
+		import("normalize-package-data"),
+		import("node:path"),
+	]);
+	const glob_packages = import.meta.glob(
+		"../../../../../packages/**/package.json",
+		{
+			import: "default",
+		},
+	);
 	const packages = new Map<string, Package>(
 		await Promise.all(
 			Object.entries(glob_packages).map(async ([pkg_path, mod]) => {
-				const data = (await mod()) as normalize.Package;
+				const data = (await mod()) as NormalizedPkgData;
 				normalize(data, true);
 				return [
 					data.name,
 					{
 						data,
-						dirname: get_pkg_dirname(pkg_path),
+						dirname: get_pkg_dirname({ input: pkg_path, path }),
 						// NOTE: We will update later
 						readme: "",
 					},
@@ -31,12 +43,15 @@ export async function load() {
 			}),
 		),
 	);
-	const glob_readmes = import.meta.glob("../../../../../packages/**/README.md", {
-		query: "?raw",
-		import: "default",
-	});
+	const glob_readmes = import.meta.glob(
+		"../../../../../packages/**/README.md",
+		{
+			query: "?raw",
+			import: "default",
+		},
+	);
 	for (const [md_path, mod] of Object.entries(glob_readmes)) {
-		const name = get_pkg_name(md_path);
+		const name = get_pkg_name({ input: md_path, path });
 		const pkg = packages.get(name);
 		if (!pkg) throw new Error(`Could not find package for: ${md_path}`);
 		const readme = (await mod()) as string;
@@ -47,12 +62,16 @@ export async function load() {
 	};
 }
 
-function get_pkg_dirname(input: string): string {
+function get_pkg_dirname(params: {
+	input: string;
+	path: typeof import("node:path");
+}): string {
+	const { input, path } = params;
 	return path.dirname(path.relative("../../../../../packages", input));
 }
 
-function get_pkg_name(input: string): string {
-	const dirname = get_pkg_dirname(input);
+function get_pkg_name(params: Parameters<typeof get_pkg_dirname>[0]): string {
+	const dirname = get_pkg_dirname(params);
 	if (PREFIX_EXCEPTIONS.has(dirname)) return dirname;
 	return `${ORG_NAME}/${dirname}`;
 }
--- apps/docs/src/routes/package/[name]/+page.server.ts
+++ apps/docs/src/routes/package/[name]/+page.server.ts
@@ -1,6 +1,6 @@
 import { error } from "@sveltejs/kit";
-import { mdToSvelte } from "@sveltepress/vite";
-import path from "pathe";
+
+import { building } from "$app/environment";
 
 export async function load(ev) {
 	const { params, parent } = ev;
@@ -8,9 +8,16 @@ export async function load(ev) {
 	const name = globalThis.decodeURIComponent(params.name);
 	const pkg = packages.get(name);
 	if (!pkg) throw error(404, `Unrecognized package name "${name}"`);
-	const html = await mdToSvelte({
-		mdContent: pkg.readme,
-		filename: path.join(pkg.dirname, "README.md"),
-	});
-	return { pkg, html };
+	if (building) {
+		const [path, sveltepress] = await Promise.all([
+			import("node:path"),
+			import("@sveltepress/vite"),
+		]);
+		const html = await sveltepress.mdToSvelte({
+			mdContent: pkg.readme,
+			filename: path.join(pkg.dirname, "README.md"),
+		});
+		return { pkg, html };
+	}
+	throw new Error("Unreachable");
 }
