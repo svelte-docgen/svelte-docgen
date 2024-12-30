055425cc4167b4bcf95934cb0bd5a43c459e4c8f
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-30 13:00:36 +0800

    add skeleton

---
+++ apps/docs/src/lib/components/ui/skeleton/skeleton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { WithElementRef, WithoutChildren } from "bits-ui";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	type Props = WithoutChildren<WithElementRef<HTMLAttributes<HTMLDivElement>>>;
+	let {
+		// Custom
+		ref = $bindable(null),
+		// Native
+		class: class_,
+		...rest_props
+	}: Props = $props();
+</script>
+
+<div
+	bind:this={ref}
+	class={["bg-primary/10 animate-pulse rounded-md", class_]}
+	{...rest_props}
+></div>
