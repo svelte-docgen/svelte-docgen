3581f2ae3b205ae3d1c282d55422286d25b0d450
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-30 13:00:39 +0800

    add tooltip

---
+++ apps/docs/src/lib/components/ui/tooltip/index.ts
@@ -0,0 +1,9 @@
+import { Tooltip } from "bits-ui";
+
+import Content from "./tooltip-content.svelte";
+
+const Root = Tooltip.Root;
+const Trigger = Tooltip.Trigger;
+const Provider = Tooltip.Provider;
+
+export { Root, Trigger, Content, Provider };
