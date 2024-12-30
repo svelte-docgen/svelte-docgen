b40ae337629b2130d0adac40b804e9780f7ad2af
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-30 11:37:20 +0800

    init mdsvex

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,20 +1,27 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @satisfies {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [".md", ".svelte"],
-
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 	},
+
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: [
+				// NOTE: The reason we use `.svelte.md` instead of `svx` is a better integration with IDEs
+				".svelte.md",
+			],
+		}),
+	],
 };
 
 export default config;
