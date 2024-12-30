5e280d17e37231ab52ec7a51ba40653058994bb8
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-30 13:00:30 +0800

    add separator

---
+++ apps/docs/src/lib/components/ui/separator/separator.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Separator } from "bits-ui";
+
+	let {
+		// Custom
+		orientation = "horizontal",
+		ref = $bindable(null),
+		// Native
+		class: class_,
+		...rest_props
+	}: Separator.RootProps = $props();
+</script>
+
+<Separator.Root
+	bind:ref
+	class={[
+		"bg-border shrink-0",
+		orientation === "horizontal" ? "h-[1px] w-full" : "min-h-full w-[1px]",
+		class_
+	]}
+	{orientation}
+	{...rest_props}
+/>
