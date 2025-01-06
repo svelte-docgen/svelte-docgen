4d1b6facd3834c9db59f79bc8d116fff2efd4749
Mateusz Kadlubowski <xeho91@pm.me>
2025-01-06 16:46:43 +0800

    wip

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -23,14 +23,18 @@ const config = {
 		},
 		extend: {
 			colors: {
+				background: "oklch(var(--background) / <alpha-value>)",
+				foreground: "oklch(var(--foreground) / <alpha-value>)",
+				accent: {
+					DEFAULT: "oklch(var(--accent) / <alpha-value>)",
+					foreground: "oklch(var(--accent-foreground) / <alpha-value>)",
+				},
 				border: "hsl(var(--border) / <alpha-value>)",
 				input: "hsl(var(--input) / <alpha-value>)",
 				ring: "hsl(var(--ring) / <alpha-value>)",
-				background: "oklch(var(--background) / <alpha-value>)",
-				foreground: "oklch(var(--foreground) / <alpha-value>)",
 				primary: {
-					DEFAULT: "oklch(var(--primary) / <alpha-value>)",
-					foreground: "oklch(var(--primary-foreground) / <alpha-value>)",
+					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+					foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
 				},
 				secondary: {
 					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
@@ -43,10 +47,6 @@ const config = {
 				muted: {
 					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
 					foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
 				},
 				popover: {
 					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
@@ -61,8 +61,8 @@ const config = {
 					foreground: "hsl(var(--sidebar-foreground))",
 					primary: "hsl(var(--sidebar-primary))",
 					"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
-					accent: "hsl(var(--sidebar-accent))",
-					"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+					accent: "oklch(var(--sidebar-accent))",
+					"accent-foreground": "oklch(var(--sidebar-accent-foreground))",
 					border: "hsl(var(--sidebar-border))",
 					ring: "hsl(var(--sidebar-ring))",
 				},
