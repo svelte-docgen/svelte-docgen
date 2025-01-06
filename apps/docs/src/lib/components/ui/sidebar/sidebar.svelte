7251605bc095dfd68eda9e9f32c369e85dde44c9
Taku Fukada <naninunenor@gmail.com>
2025-01-06 14:02:35 +0900

    update dependencies

--- apps/docs/src/lib/components/ui/sidebar/sidebar.svelte
+++ apps/docs/src/lib/components/ui/sidebar/sidebar.svelte
@@ -32,7 +32,7 @@
 		{@render children?.()}
 	</div>
 {:else if sidebar.isMobile}
-	<Sheet.Root controlledOpen open={sidebar.openMobile} onOpenChange={sidebar.setOpenMobile} {...restProps}>
+	<Sheet.Root bind:open={() => sidebar.openMobile, (v) => sidebar.setOpenMobile(v)} {...restProps}>
 		<Sheet.Content
 			data-sidebar="sidebar"
 			data-mobile="true"
