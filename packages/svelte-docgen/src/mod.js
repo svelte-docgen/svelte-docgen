4de63c1b4e2caaa64c7e0c31ef0c4ae0cae2e3e0
Mateusz Kadlubowski <xeho91@pm.me>
2025-01-07 17:43:43 +0800

    improve library exports

--- packages/svelte-docgen/src/mod.js
+++ packages/svelte-docgen/src/mod.js
@@ -1,6 +1,9 @@
+// External
 export { createCacheStorage } from "@svelte-docgen/extractor";
+// Internal
 export { analyze } from "./analyzer/mod.js";
+export { decode, encode } from "./codec.js";
+export { BASE_TYPE_KIND, ADVANCED_TYPE_KIND, INSTANTIABLE_TYPE_KIND } from "./kind/core.js";
+export * as is from "./kind/mod.js";
 export { Options } from "./options.js";
 export { parse } from "./parser/mod.js";
-export { decode, encode } from "./codec.js";
-// export { isTypeRef } from "./doc/utils.js"; // FIXME: dts-buddy doesn't support type guard
