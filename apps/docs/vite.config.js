80bd7cfaeede21046eb3caac9896b83d99181ec6
Mateusz Kadlubowski <xeho91@pm.me>
2025-01-20 07:36:54 +0800

    remove unused directive

--- apps/docs/vite.config.js
+++ apps/docs/vite.config.js
@@ -1,7 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 
-// @ts-expect-error FIXME: Upstream issue
 export default defineConfig((env) => {
 	return {
 		assetsInclude: ["../../assets/**/*"],
